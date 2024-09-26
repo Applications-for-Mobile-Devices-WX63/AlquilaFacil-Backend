af77a4449d8f17222144dc54fb5c0ff267f8f634
Tato <italoluna23@gmail.com>
2024-09-26 03:58:21 -0500

    fix: program file for use swagger

--- AlquilaFacilPlatform/Program.cs
+++ AlquilaFacilPlatform/Program.cs
@@ -224,7 +224,7 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "AlquilaFacil.API v1");
-        c.RoutePrefix = string.Empty;
+        c.RoutePrefix = "swagger"; // Acceder a Swagger solo desde "/swagger"
     });
 }
 
